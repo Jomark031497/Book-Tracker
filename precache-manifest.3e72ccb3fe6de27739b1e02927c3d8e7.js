91a1cb42cc93c9aec9b7bdb734df1a3e05e81195
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-08-07 11:52:01 +0800

    Updates

---
+++ precache-manifest.3e72ccb3fe6de27739b1e02927c3d8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a97fd1cc16961f2f3a121cd9a762bd3",
+    "url": "/Book-Tracker/index.html"
+  },
+  {
+    "revision": "86c513098554bddf6e14",
+    "url": "/Book-Tracker/static/css/main.bcefbdd7.chunk.css"
+  },
+  {
+    "revision": "7d6753525a17d327dbb5",
+    "url": "/Book-Tracker/static/js/2.d3eca2e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Book-Tracker/static/js/2.d3eca2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c513098554bddf6e14",
+    "url": "/Book-Tracker/static/js/main.4d9af50c.chunk.js"
+  },
+  {
+    "revision": "858f75a2044b405adf8a",
+    "url": "/Book-Tracker/static/js/runtime-main.c5a2fdb0.js"
+  }
+]);
